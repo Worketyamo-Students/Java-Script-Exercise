e72bbe88478e5ef1358d4f6b9a38e371d13bcc6d
Zackyy97 <zackgilliam58@gmail.com>
2025-02-28 18:42:15 +0100

    v2

--- exo.js
+++ exo.js
@@ -1,88 +1,58 @@
-1
-// var nom 
-// const age = 44
-// let nom = prompt("veulliez entrez le nom de lutilisateur")
-// let age = prompt("veulliez entrez le age de lutilisateur")
-// console.log("bienvenue Mme Farel")
-// 2
+// Exercise: 1
+// let userName;
+// const UserAge = prompt("Quel age aviez vous");
+// userName = prompt("what is your Name :")
+// // UserAge = prompt("what is your Age")
+// console.log("Bienvenue", userName, "Vous avez", UserAge)
 
-// let tranche1=prompt("veuillez entrez le premiere montant")
-// let tranche2=prompt("veuillez entrez le deuxieme montant")
-// function prix_total(tranche1,tranche2){
-//     return tranche1 + tranche2
+// Exercise: 2
+// function calculerlecouttotal(coutparparticipant, lenombredeparticipant) {
+// let couttotal = coutparparticipant*lenombredeparticipant;
+// return console.log("le cout total est de" , couttotal,"franc CFA" )
 // }
-// console.log(typeof tranche1)
+//  calculerlecouttotal(50,10)
 
-function calculerCoutTotal(coutParParticipant, nombreParticipants) {
-    return coutParParticipant * nombreParticipants;
-  }
-  
-  // Exemple d'utilisation :
-  let coutParParticipant = 10;
-  let nombreParticipants = 5;
-  let coutTotal = calculerCoutTotal(coutParParticipant, nombreParticipants);
-  console.log("Le coût total est de : " + coutTotal);
+//  Exercise : 3
 
-//   3
-function verifierEligibiliteReduction(age) {
-    if (age < 12) {
-      console.log("Vous êtes éligible pour une réduction enfant.");
-    } else if (age >= 12 && age < 18) {
-      console.log("Vous êtes éligible pour une réduction adolescent.");
-    } else {
-      console.log("Vous n'êtes pas éligible pour une réduction.");
-    }
-  }
-  
-  // Exemple d'utilisation :
-  verifierEligibiliteReduction(10); 
-  verifierEligibiliteReduction(15); 
-  verifierEligibiliteReduction(20);
-//   4
-// Demander les notes à l'utilisateur
-let note1 = prompt("Veuillez entrer la première note :");
-let note2 = prompt("Veuillez entrer la deuxième note :");
-let note3 = prompt("Veuillez entrer la troisième note :");
-// Convertir les notes en nombres (car prompt renvoie des chaînes de caractères)
-note1 = parseFloat(note1);
-note2 = parseFloat(note2);
-note3 = parseFloat(note3);
+// let a = prompt("Enter your age")
+//  function Eligible(a) {
+//      if(a>18 && a<=60){
+//         console.log("Bienvenue monsiuer que pouvons nous faire pour vous")
+//     }
+    
+//     else{
+//         console.log("Votre age et nonvalid")
+//     }
+// }
+//   Eligible(a)
+Exercise : 4
 
-// Calculer la moyenne
-let moyenne = (note1 + note2 + note3) / 3;
+let note1 = prompt("entrez la premiere note 1"); 
+console.log("Note numero 1 et de :" , note1)
+let note2 = prompt("entrez la premiere note 2");
+console.log("Note numero 2 et de :" , note2)
+let note3 = prompt("entrez la premiere note 3");
+console.log("Note numero 3 et de :" , note3)
 
-// Afficher le résultat
-console.log("La moyenne des notes est de : " + moyenne);
+var somme = Math.max(note1,0) + Math.max(note2,0)+ Math.max(note3,0)
+// console.log("Bonjour " + note1 + " !"); 
+console.log("la somme des note et de :" , somme )
 
-// 5
-// Déclarer et initialiser les variables
-let nom = "John Doe";
-let age = 30;
-let inscriptionPayee = true;
+// Exercise : 5
 
-// Afficher les types des variables
-console.log("Type de la variable nom : " + typeof nom); // Affiche "string"
-console.log("Type de la variable age : " + typeof age); // Affiche "number"
-console.log("Type de la variable inscriptionPayee : " + typeof inscriptionPayee); // Affiche "boolean"
+// let name = prompt("Inserez votre nom :");
+// console.log("your name is : ", name)
+// let age = Number(prompt("insert your age :"));
+// console.log("you are",age,"old")
+// let fees = Number(prompt("how much did you pay for school fee"))
+// console.log("You have pay:",fees,"$")
+// if (name = String){
+//     console.log(typeof name)
+// }
+// if (age= numer){
+//     console.log(typeof age)
+// }
 
-// 6
-// Créer un tableau avec des noms de participants
-let participants = ["Alice", "Bob", "Charlie"];
 
-// Ajouter un nouveau nom à la fin du tableau
-participants.push("David");
 
-// Afficher tous les noms du tableau
-console.log("Liste des participants :");
-for (let i = 0; i < participants.length; i++) {
-  console.log(participants[i]);
-}
-// 7
-// Demander le nom du nouvel inscrit
-let nomNouvelInscrit = prompt("Veuillez entrer le nom du nouvel inscrit :");
 
-// Afficher une alerte de bienvenue
-alert("Bienvenue, " + nomNouvelInscrit + " !");
-
-// Afficher un message de bienvenue dans la console
-console.log("Un nouvel inscrit est arrivé : " + nomNouvelInscrit);
